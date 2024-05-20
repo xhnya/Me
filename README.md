ffc376f198fe40e05f9cfc7994079a46a08672bd
肖海南 <hain.work@outlook.com>
2024-05-20 16:00:13 +0800

    更改

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
-# Vue 3 + TypeScript + Vite
+# 个人简介
 
-This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+我是一枚全栈开发者
+邮箱: hain.xiao@outlook.com
+## 技术栈
+### 前端
+- HTML
+- CSS
+- JavaScript
+- Vue
+- Node.js
+### 后端
+- Java
+- SpringBoot SpringMVC MyBatis
+- SpringCloud SpringCloudAlibaba Dubbo Zookeeper
+- MySQL
+### 中间件
+- RabbitMQ
+- Redis
+- Elasticsearch
+- Nginx
+- Docker
 
-## Recommended Setup
+## 开源项目
+### 游戏社区
+后端代码  [点击跳转](https://github.com/xhnya/light)
+前端代码 [点击跳转](https://github.com/xhnya/light-front)
+后台管理代码 [点击跳转](https://github.com/xhnya/light-admin)
 
-- [VS Code](https://code.visualstudio.com/) + [Vue - Official](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (previously Volar) and disable Vetur
+项目描述：light是一个游戏社区，采用SpringCloud &Alibaba,SpringBoot 2.3,Mybatis,Elasticsearch,Docker等核心技术，提供了前台，管理后台页面。集成了注册中心，配置中心，链路追踪，网关
+第三方服务：阿里云OSS,阿里云短信验证码,阿里云视频点播,邮件服务
+### 博客
+代码 [点击跳转](https://github.com/xhnya/blog)
+使用技术：SpringBoot,Mybatis,MySQL,thymeleaf
 
-- Use [vue-tsc](https://github.com/vuejs/language-tools/tree/master/packages/tsc) for performing the same type checking from the command line, or for generating d.ts files for SFCs.
+### 健身APP
+代码 [点击跳转](https://gitee.com/xhnya/exercise)
+使用技术 UniApp,Vue,Node.js
+
+
+## 项目经验
+### 教育网
+项目描述：教育网是一个教育类网站，采用SpringBoot,Mybatis,MySQL等技术，提供了前台，管理后台页面。集成了注册中心，配置中心，链路追踪，网关
+包括了OSS文件上传，短信验证码,阿里云视频点播等功能
+
+### QQ邮箱测试项目
+使用python+selenium编写的自动化测试项目，实现了QQ邮箱的登录，发送邮件，查看邮件等功能
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,88 +1,120 @@
 <template>
   <div class="index-style">
-    <div>
-      <el-descriptions
-          class="margin-top"
-          title="With border"
-          :column="3"
-          border
-      >
-        <el-descriptions-item>
-          <template #label>
-            <div class="cell-item">
-              <el-icon>
-                <user />
-              </el-icon>
-              用户名
-            </div>
-          </template>
-          xhn
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template #label>
-            <div class="cell-item">
-              <el-icon>
-                <iphone />
-              </el-icon>
-              邮箱
-            </div>
-          </template>
-          hain.xiao@outlook.com
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template #label>
-            <div class="cell-item">
-              <el-icon>
-                <location />
-              </el-icon>
-              地址
-            </div>
-          </template>
-          南昌
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template #label>
-            <div class="cell-item">
-              <el-icon>
-                <tickets />
-              </el-icon>
-              Remarks
-            </div>
-          </template>
-          <el-tag size="small">School</el-tag>
-        </el-descriptions-item>
-        <el-descriptions-item>
-          <template #label>
-            <div class="cell-item">
-              <el-icon>
-                <office-building />
-              </el-icon>
-              Address
-            </div>
-          </template>
-          No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
-        </el-descriptions-item>
-      </el-descriptions>
+    <h1>个人简介</h1>
+    <p>我是一枚全栈开发者</p>
+    <p>邮箱: <a href="mailto:hain.xiao@outlook.com">hain.xiao@outlook.com</a></p>
 
+    <h2>技术栈</h2>
+    <div class="tech-stack">
+      <h3>前端</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Vue</li>
+        <li>Node.js</li>
+      </ul>
+
+      <h3>后端</h3>
+      <ul>
+        <li>Java</li>
+        <li>SpringBoot, SpringMVC, MyBatis</li>
+        <li>SpringCloud, SpringCloudAlibaba, Dubbo, Zookeeper</li>
+        <li>MySQL</li>
+      </ul>
+
+      <h3>中间件</h3>
+      <ul>
+        <li>RabbitMQ</li>
+        <li>Redis</li>
+        <li>Elasticsearch</li>
+        <li>Nginx</li>
+        <li>Docker</li>
+      </ul>
+    </div>
+
+    <h2>开源项目</h2>
+    <div class="projects">
+      <h3>游戏社区</h3>
+      <p>
+        后端代码 <a href="https://github.com/xhnya/light" target="_blank">点击跳转</a> |
+        前端代码 <a href="https://github.com/xhnya/light-front" target="_blank">点击跳转</a> |
+        后台管理代码 <a href="https://github.com/xhnya/light-admin" target="_blank">点击跳转</a>
+      </p>
+      <p>项目描述：light是一个游戏社区，采用SpringCloud & Alibaba, SpringBoot 2.3, Mybatis, Elasticsearch, Docker等核心技术，提供了前台，管理后台页面。集成了注册中心，配置中心，链路追踪，网关。第三方服务：阿里云OSS, 阿里云短信验证码, 阿里云视频点播, 邮件服务。</p>
+
+      <h3>博客</h3>
+      <p>
+        代码 <a href="https://github.com/xhnya/blog" target="_blank">点击跳转</a><br>
+        使用技术：SpringBoot, Mybatis, MySQL, thymeleaf
+      </p>
+
+      <h3>健身APP</h3>
+      <p>
+        代码 <a href="https://gitee.com/xhnya/exercise" target="_blank">点击跳转</a><br>
+        使用技术 UniApp, Vue, Node.js
+      </p>
+    </div>
+
+    <h2>项目经验</h2>
+    <div class="projects">
+      <h3>教育网</h3>
+      <p>
+        项目描述：教育网是一个教育类网站，采用SpringBoot, Mybatis, MySQL等技术，提供了前台，管理后台页面。集成了注册中心，配置中心，链路追踪，网关。包括了OSS文件上传，短信验证码, 阿里云视频点播等功能。
+      </p>
+
+      <h3>QQ邮箱测试项目</h3>
+      <p>
+        使用python+selenium编写的自动化测试项目，实现了QQ邮箱的登录，发送邮件，查看邮件等功能。
+      </p>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
-import {
-  Iphone,
-  Location,
-  OfficeBuilding,
-  Tickets,
-  User,
-} from '@element-plus/icons-vue'
-
-
-
+// 这里可以添加任何需要的 TypeScript 逻辑，目前不需要额外的逻辑
 </script>
+
 <style scoped>
 .index-style {
   padding: 20px;
-  margin-right: 15%;
-  margin-left: 15%;
+  margin: 0 auto;
+  max-width: 800px;
+  background-color: #f9f9f9;
+  border-radius: 10px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  font-family: Arial, sans-serif;
+}
+
+.index-style h1, .index-style h2, .index-style h3 {
+  color: #333;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+.index-style p {
+  color: #666;
+  line-height: 1.5;
+  margin-bottom: 10px;
+}
+
+.index-style ul {
+  color: #666;
+  line-height: 1.5;
+  list-style-type: disc;
+  margin: 0 0 10px 20px;
+}
+
+.index-style a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+.index-style a:hover {
+  text-decoration: underline;
+}
+
+.tech-stack, .projects {
+  margin-bottom: 20px;
 }
 </style>
